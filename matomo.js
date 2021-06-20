2295dc329e66f25dfca2bd59db0de7af5b451910
Adriano Heller Mylla <adriano@technoizz.com>
2021-06-20 01:39:47 -0300

    feat: Add Matomo Tracker

--- matomo.js
+++ matomo.js
@@ -1,9 +1,9 @@
 const MatomoTracker = require('matomo-tracker');
 
-let Matomo = new MatomoTracker(1,'https://hms-hub-8bfda.web.app/')
+let Matomo = new MatomoTracker(1,'https://wgcompany.systems');
 
 Matomo.on('error', err => {
     console.error(`Error tracking Request:${err}`);
 });
 
-Matomo.track('https://hms-hub-8bfda.web.app/');
+Matomo.track('https://wgcompany.systems');
